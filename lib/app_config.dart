637c52f786da5122d142feb2b6bb5a7a07b9181f
PiyushAdy <piyushady125@gmail.com>
2025-08-09 23:31:47 +0530

    Update app_config.dart

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -5,6 +5,6 @@ class AppConstants {
 }
 
 class ApiKeys {
-  static const String geminiApiKey = "AIzaSyAKqGP-DucS5WpsSooFmR01zNKiT88xHsw"; 
-  static const String openWeatherApiKey = "e509072ec61b70d60254229dd28cf92b"; 
+  static const String geminiApiKey = "GEMINI_API_KEY"; 
+  static const String openWeatherApiKey = "OPENWEATHER_API_KEY"; 
 }
